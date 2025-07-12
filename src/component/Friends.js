ccad8471544a306b8109af9275a91c1bf6574ad5
Proton <basselawni1@gmail.com>
2025-07-12 21:50:08 +0300

    This is kinda of a stupid project but hey! a work is a work

--- src/component/Friends.js
+++ src/component/Friends.js
@@ -10,12 +10,29 @@ function Friend({ person }) {
 
 function Friends() {
   const [friends, setFriends] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
+  const handleAddFriendClick = () => {
+    setShowPopup(true);
+  };
+  const closePopup = () => {
+    setShowPopup(false);
+  };
   return (
     <div className="Friends">
-      <button>Add friends</button>
-      {friends.map((person, index) => {
-        <Friend key={index} person={person} />;
-      })}
+      <button onClick={handleAddFriendClick}>Add friends</button>
+      {friends.map((person, index) => (
+        <Friend key={index} person={person} />
+      ))}
+      {showPopup && (
+        <div>
+          <div className="popup-overlay" onClick={closePopup}>
+            <div className="popup-content" onClick={(e) => e.stopPropagation()}>
+              <h3>Add New Friend</h3>
+              <button onClick={closePopup}>Close</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
